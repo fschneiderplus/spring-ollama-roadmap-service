c0c5e690f2556dbb13d56bb21c2e1a0d809b2e36
Pajic <petar.pajic@stud.sbg.ac.at>
2025-01-25 16:43:05 +0100

    test

--- src/main/java/com/example/demo/RoadmapNodeRepository.java
+++ src/main/java/com/example/demo/RoadmapNodeRepository.java
@@ -5,5 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoadmapNodeRepository extends JpaRepository<RoadmapNode, Long> {
+
+    System.println.out("asd");
     
 }
